732c41d2d00492c75a2efe3e3e693e5a7df2c213
zhangjx <zhangjx@DESKTOP-3PQT5RM>
2018-12-15 13:40:38 +0800

    init

---
+++ src/main/java/com/wdcloud/test/StartApp.java
@@ -0,0 +1,24 @@
+package com.wdcloud.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 文件名称： com.wdcloud.test.StartApp.java</br>
+ * 初始作者： Administrator</br>
+ * 创建日期： 2017年12月9日</br>
+ * 功能说明： 这里用一句话描述这个类的作用--此句话需删除 <br/>
+ * =================================================<br/>
+ * 修改记录：<br/>
+ * 修改作者 日期 修改内容<br/>
+ * ================================================<br/>
+ * Copyright (c) 2010-2011 .All rights reserved.<br/>
+ */
+@SpringBootApplication
+public class StartApp {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StartApp.class, args);
+	}
+}
---
+++ src/main/java/com/wdcloud/test/controller/TestAction.java
@@ -0,0 +1,25 @@
+package com.wdcloud.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 文件名称： com.wdcloud.test.action.TestAction.java</br>
+ * 初始作者： Administrator</br>
+ * 创建日期： 2017年12月9日</br>
+ * 功能说明： 这里用一句话描述这个类的作用--此句话需删除 <br/>
+ * =================================================<br/>
+ * 修改记录：<br/>
+ * 修改作者 日期 修改内容<br/>
+ * ================================================<br/>
+ * Copyright (c) 2010-2011 .All rights reserved.<br/>
+ */
+@RestController
+public class TestAction {
+
+	@GetMapping(value = "test")
+	public Object test() {
+
+		return null;
+	}
+}
