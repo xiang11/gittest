5b4d49cb2192241d034c743dfeb45a0b2af537d3
zhangjx <zhangjx@DESKTOP-3PQT5RM>
2018-12-15 13:25:32 +0800

    init

---
+++ src/com/wd/test/test.java
@@ -0,0 +1,25 @@
+package com.wd.test;
+
+/**
+ * 文件名称： com.wd.test.test.java</br>
+ * 初始作者： zhangjianxiang</br>
+ * 创建日期： 2018年12月15日</br>
+ * 功能说明： <br/> 
+ * 
+ * =================================================<br/> 
+ * 修改记录：<br/> 
+ * 修改作者        日期       修改内容<br/> 
+ * 
+ * 
+ * ================================================<br/> 
+ *  Copyright (c) 2010-2011 .All rights reserved.<br/> 
+ */
+public class test {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
